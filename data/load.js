6ab3b591e8e653aef8511c0018ed1d5a1be0e8ff
Lucas Ramos Romagnoli <lucasr.romagnoli@gmail.com>
2020-11-10 14:20:52 -0300

    Inserindo texto 2 do 'the endless tale'

--- data/load.js
+++ data/load.js
@@ -72,6 +72,13 @@ TextAndAudio.Manage = (function () {
                     "In the Far East there was a Great King who had no work to do. Every day, andall day long, he sat on soft cushions and listened to stories. And no matterwhat the story was about, he never grew tired of hearing it, even thoughmany of the stories took hours to complete. “There is only one fault with yourstory,” he often said, “it is too short.” All the storytellers in the world wereinvited to his palace; and some of them told stories that were very longindeed."
                 ],
                 audio: "theendlesstalepart1.mp3"
+            },
+            {
+                title: "The Endless Tale Part 2",
+                text: [
+                    "But The King was always sad when the story ended. At last he sent a message to every city and town, offering a prize to any one able to tell him an endless tale. He said: “To the man that will tell me a story which lasts forever, I will give my daughter as his wife; and I will make him my heir, and he shall be king after me.” But this challenge wasn’t quite what it seemed. The King added a strict condition. “If any man tries to tell such a story and fails, then his head will be cut off.”."
+                ],
+                audio: "theendlesstalepart2.mp3"
             }
         ]
 
